f51d02aa0c976e18483919b3fc3aa3d836de8215
Finley <finley.mwachia12@gmail.com>
2024-02-18 15:56:32 +0300

    added variable folder and files

---
+++ variables/variable_test.py
@@ -0,0 +1,6 @@
+# Write the variables for:
+# We check in a patient named John Smith.He is 20 years old and is a new patient.
+
+patient_name = 'John Smith'
+age = 20
+is_new_patient= True
---
+++ variables/variables.py
@@ -0,0 +1,12 @@
+price = 10#variable name
+price = 20#interger
+rating = 4.9# float
+name = "Finley"#text
+is_published = False#boolean
+
+print(price)#print function
+
+#features of variables
+# 1.should start with small letter
+# 2.more words are joined usin underscore
+# 3.do not start with a number or special symbols
