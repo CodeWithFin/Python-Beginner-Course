f5f92176bfd15873f00a9f19fa3ea4868791c83c
Finley <finley.mwachia12@gmail.com>
2024-02-27 18:10:43 +0300

    added type conversion folder together with its files

---
+++ type_conversion/test_type_conversion.py
@@ -0,0 +1,4 @@
+# Ask a user their weight(in pounds), converst it to kilograms and print it on the terminal.
+weight_pounds = int(input("What is your Weight in pounds?  "))
+weight_kilograms = weight_pounds * 0.45
+print(f'Your Weight in kilograms is {weight_kilograms} kg.')
---
+++ type_conversion/type_conversion.py
@@ -0,0 +1,3 @@
+birth_year = int(input('Birth year: '))
+age = 2024- birth_year
+print(age)
