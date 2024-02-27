564c54fef717e7b26c55150fbad6bb88dd3a6b1c
Finley <finley.mwachia12@gmail.com>
2024-02-27 18:51:33 +0300

    added the arithmetic operator folder and its files

---
+++ Arithmetic Operations/arithmetic_operations.py
@@ -0,0 +1,12 @@
+print(10+3)#adds 10 and 3 to return 13
+print(10/3)#divides the two numbers to return 3.3333
+print(10//3)#divides the two numbers to return 3(an integer)
+print(10%3)#divides the two numbers and returns the reminder
+print(10**3)#returns 10 to the power of 3 = 1000
+
+
+# augumented operator
+x=10
+x+=3
+print(x)
+
---
+++ Arithmetic Operations/operator_precedence.py
@@ -0,0 +1,6 @@
+x = 10 + 3 * 2#operator precendence is like BODMAS.
+print(x)
+
+# exponentiation
+# multiplication or division
+# addition or subtraction
