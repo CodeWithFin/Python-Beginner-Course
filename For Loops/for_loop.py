40a6c45dbc6f9ce3c6ccc3d32493289228e56ce4
Finley <finley.mwachia12@gmail.com>
2024-03-03 08:52:14 +0300

    add for loop folder and its files

---
+++ For Loops/for_loop.py
@@ -0,0 +1,6 @@
+for item in ['Finley','Mwangola','Mwachia']: #item is the for loop variable which will hold one item at a time
+    print(item)
+
+for number in range(1,10,2):
+    print(number)
+
---
+++ For Loops/for_loop_exercise.py
@@ -0,0 +1,5 @@
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total += price
+print(total)
