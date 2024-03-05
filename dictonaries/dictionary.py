6cd444294b1c678298235e8fc18075fb18a33800
Finley <finley.mwachia12@gmail.com>
2024-03-05 08:33:53 +0300

    add dictionary folder and its files

---
+++ dictonaries/dictionary.py
@@ -0,0 +1,9 @@
+customer = {
+    'name' : 'Finley Mwangola',
+    'is_verified' : True,
+    'age': 21
+}
+ 
+print(customer['name'])
+print(customer.get('name'))#this one is better
+customer['name'] = 'Shenaila Wangare'#to update a key
---
+++ dictonaries/dictionary_exercise.py
@@ -0,0 +1,20 @@
+phone = input("Phone: ")
+
+digits_mapping = {
+    "0" : 'zero',
+    "1" : "one",
+    "2" : 'two',
+    "3" : 'three',
+    "4": 'four',
+    "5" : 'five',
+    "6" : 'six',
+    "7": 'seven',
+    "8" : 'eight',
+    "9" : 'nine'
+}
+output = ""
+for ch in phone:
+    output += digits_mapping.get(ch,"!") + " "
+
+print(output)
+
